757e51fb3e9ca818c1278dd83c7bad3943e51c9a
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-09-10 17:13:34 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import * as path from 'path'
-import {defineConfig} from "vite";
+import * as path from "path";
+
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-})
+});
