5f340c5e716e94f3a0f2cf6df71e6b18af5deba0
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-11-01 17:48:02 +0100

    feat: add Dropdown, Avatar, Edit, Header, Logo, PersonOutline components

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Logo } from './logo'
+export { default as PersonOutline } from './person-outline'
+export { default as Edit } from './edit'
+export { default as Logout } from './logout'
