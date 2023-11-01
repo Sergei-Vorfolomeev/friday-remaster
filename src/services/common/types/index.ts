4097440ac48c8cd0c1445ae023741a5d33e9b646
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-11-01 18:04:23 +0100

    feat: add table component

---
+++ src/services/common/types/index.ts
@@ -0,0 +1,22 @@
+export type Pagination = {
+  totalPages: number
+  currentPage: number
+  itemsPerPage: number
+  totalItems: number
+}
+
+export type Paginated<T> = {
+  pagination: Pagination
+  items: T[]
+}
+export type Sort = {
+  key: string
+  direction: 'asc' | 'desc'
+} | null
+
+export type PaginatedArgs<T extends Record<string, any> = {}> = {
+  currentPage?: number
+  itemsPerPage?: number
+  pageSize?: number
+  orderBy?: string | null
+} & T
